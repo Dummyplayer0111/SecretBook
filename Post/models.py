b2d5fd0a56fff92cc81ff367024b329d5e1da5ba
Dummyplayer0111 <213612620+Dummyplayer0111@users.noreply.github.com>
2025-06-11 14:44:08 +0530

    Temp changes

--- Post/models.py
+++ Post/models.py
@@ -8,7 +8,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, null=True)
+
 
 
 
