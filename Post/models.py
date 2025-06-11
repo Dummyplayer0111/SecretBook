cdeeff78ff564e63dfd7e39f19ab00d1a88f3591
Dummyplayer0111 <213612620+Dummyplayer0111@users.noreply.github.com>
2025-06-11 16:06:10 +0530

    Temp changes 4

--- Post/models.py
+++ Post/models.py
@@ -8,7 +8,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
 
     def __str__(self):
         return self.title   
