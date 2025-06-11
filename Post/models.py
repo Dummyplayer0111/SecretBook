f4c78b287d3eb4ca088028700cb094432dad5ccc
Dummyplayer0111 <213612620+Dummyplayer0111@users.noreply.github.com>
2025-06-11 17:39:06 +0530

    More changes

--- Post/models.py
+++ Post/models.py
@@ -8,8 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
-    uid = models.UUIDField(null=True, blank=True, unique=True, editable=False)
-
+    uid = models.UUIDField(default=uuid.uuid4, unique=True)
 
 
 
