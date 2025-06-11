740338036c1426c4cba0956b06b8ef386260ecdd
Dummyplayer0111 <213612620+Dummyplayer0111@users.noreply.github.com>
2025-06-11 14:31:34 +0530

    Added UUID to posts

--- Post/urls.py
+++ Post/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('profile/post_new', views.new_post, name = 'post_new'),
-    path('post/<int:pn>/', views.post_detail, name ='post_detail'),
-    path('post_edit/<int:pn>/', views.post_edit, name ='post_edit'),
+    path('post/<uuid:uuid>/', views.post_detail, name ='post_detail'),
+    path('post_edit/<uuid:uuid>/', views.post_edit, name ='post_edit'),
     path('feed/', views.feed, name ='feed'),
-    path('post_deleted/<int:pn>/', views.post_deleted , name ='delete')
+    path('post_deleted/<uuid:uuid>/', views.post_deleted , name ='delete')
 ]
